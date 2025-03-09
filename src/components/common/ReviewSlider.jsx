2647ee44021da14ab547ea8c1d904e241d255e66
krishan kumar <kumarkrishan05530@gmail.com>
2025-03-09 20:52:24 +0530

    chnages in review slider

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -1,89 +1,129 @@
+// import React, { useEffect, useState } from 'react'
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/free-mode";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import { FreeMode, Pagination, Autoplay } from "swiper/modules"; // Import modules
+// import ReactStars from "react-rating-stars-component";
+// import { apiConnector } from '../../services/apiconnector';
+// import { ratingsEndpoints } from '../../services/apis';
+// import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
+
+// const ReviewSlider = () => {
+//   const [reviews, setReviews] = useState([]);
+//   const truncatWords = 15;
+
+//   useEffect(() => {
+
+//     const fetchAllReviews = async () => {
+//       const { data } = await apiConnector("GET", ratingsEndpoints.REVIEWS_DETAILS_API);
+//       console.log("lOGGING RESPONSE IN FETCH ALL REVIW", data);
+ 
+
+//       if (data?.success) {
+//         console.log("insode this");
+//         setReviews(data.data);
+//       }
+
+//       console.log("Printing review", reviews);
+//     }
+//     fetchAllReviews();
+//   }, []);
+  
+
+//   return (
+//     <div className='text-white'>
+//         <div className='h-[190px] max-w-maxContent'>
+//             <Swiper
+//             slidesPerView={4}
+//             spaceBetween={24}
+//             loop={true}
+//             freeMode={true}
+//             autoplay={{
+//                 delay: 2500,
+//             }}
+//             modules={[FreeMode, Pagination, Autoplay]}
+//             className='w-full'
+//             >
+
+//                 {
+//                     reviews.map((review, index) => (
+//                         <SwiperSlide key={index}>
+//                             <img
+//                             src={review?.user?.image
+//                              ? review?.user?.image
+//                               : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`}
+//                               alt='Profile Pic'
+//                               className='h-9 w-9 object-cover rounded-full'
+//                             />
+//                             <p>{review?.user?.firstName} {review?.user?.lastName}</p>
+//                             <p>{review?.course?.courseName}</p>
+//                             <p>
+//                                 {review?.review}
+//                             </p>
+//                             <p>{review?.rating.toFixed(1)}</p>
+//                             <ReactStars 
+//                                 count={5}
+//                                 value={review.rating}
+//                                 size={20}
+//                                 edit={false}
+//                                 activeColor="#ffd700"
+//                                 emptyIcon={<FaStar />}
+//                                 fullIcon={<FaStar />}
+//                             />
+//                         </SwiperSlide>
+//                     ))
+//                 }
+
+//             </Swiper>
+//         </div>
+//     </div>
+//   )
+
+
+// }
+
+// export default ReviewSlider
+
+
+
 import React, { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { FreeMode, Pagination, Autoplay } from "swiper/modules"; // Import modules
-import ReactStars from "react-rating-stars-component";
-import { apiConnector } from '../../services/apiconnector';
-import { ratingsEndpoints } from '../../services/apis';
-import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import { Pagination } from 'swiper/modules';
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+import { Autoplay } from 'swiper/modules';
+import { FreeMode } from 'swiper/modules';
+import {Navigation}  from 'swiper'
+import ReactStars from "react-rating-stars-component"
+import { apiConnector } from '../../services/apiconnector'
+import { ratingsEndpoints } from '../../services/apis'
+import { FaStar } from 'react-icons/fa'
+
 
 const ReviewSlider = () => {
-  const [reviews, setReviews] = useState([]);
-  const truncatWords = 15;
 
-  useEffect(() => {
+    const [reviews, setReviews] = useState([]);
+    const truncateWords = 15;
 
-    const fetchAllReviews = async () => {
-      const { data } = await apiConnector("GET", ratingsEndpoints.REVIEWS_DETAILS_API);
-      console.log("lOGGING RESPONSE IN FETCH ALL REVIW", data);
-      // console.log("lOGGING RESPONSE IN FETCH ALL REVIW2", data.data);
-      // console.log("lOGGING RESPONSE IN FETCH ALL REVIW3", data?.data?.data);
 
-      if (data?.success) {
-        console.log("insode this");
-        setReviews(data.data);
-      }
+    useEffect(() => {
+        const fetchAllReviews = async() => {
+            const {data} = await apiConnector("GET", ratingsEndpoints.REVIEWS_DETAILS_API)
+            console.log("LOgging response in rating", data);
 
-      console.log("Printing review", reviews);
-    }
-    fetchAllReviews();
-  }, []);
-  // return (
-  //   <div className='text-white'>
-  //     <div className='h-[190px] max-w-maxContent'>
-  //       <Swiper
-  //         slidesPerView={4}
-  //         spaceBetween={24}
-  //         loop={true}
-  //         FreeMode={true}
-  //         autoplay={{
-  //           delay: 2500,
+            if(data?.success) {
+                setReviews(data?.data);
+            }
 
-  //         }}
-  //         modules={[FreeMode, Pagination, Autoplay]}
-  //         className='w-full'>
+            console.log("Printing Reviews", reviews);
 
-  //         {
-  //           reviews.map((review, index) => (
-  //             // console.log("review inside", review);
-  //             // console.log("review firstanme", review?.user?.firstName);
-  //             <SwiperSlide key={index}>
-               
-  //               <img
-  //                 src={review?.user?.image
-  //                   ? review?.user?.image : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`}
-  //                 alt='profile pic'
-  //                 className='h-9 w-9 object-cover rounded-full'
-  //               />
-  //               <p>{review?.user?.firstName}</p>
-  //               <p>{review?.user?.lastName}</p>
+        }
+        fetchAllReviews();
+    }, []);
 
-  //               <p>{review?.course?.courseName}</p>
-  //               <p>
-  //                 {review?.review}
-  //               </p>
-  //               <p>{review.rating.toFixed(1)}</p>
-  //               <ReactStars
-  //                 count={5}
-  //                 size={24}
-  //                 value={review.rating}
-  //                 edit={false}
-  //                 activeColor="#ffd700"
-  //                 emptyIcon={<FaStar/>}
-  //                 halfIcon={<FaStarHalfAlt/>}
-  //               />
-  //             </SwiperSlide>
-  //           ))
-  //         }
-  //       </Swiper>
-
-  //     </div>
-
-  //   </div>
-  // )
 
   return (
     <div className='text-white'>
@@ -133,8 +173,6 @@ const ReviewSlider = () => {
         </div>
     </div>
   )
-
-
 }
 
 export default ReviewSlider
