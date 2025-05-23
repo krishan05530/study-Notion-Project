285987235e579f93a620b5f45c6d94255700469c
krishan kumar <kumarkrishan05530@gmail.com>
2025-05-23 23:54:47 +0530

    Remove server/.env from git tracking

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -10,7 +10,8 @@ const User = require("../models/User")
 const OTP = require("../models/OTP")
 const Profile =require("../models/Profile");
 const otpGenerator = require("otp-generator");
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
